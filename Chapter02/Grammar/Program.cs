813a4c544346d4de54fca7605fdc67dfdfca4624
Marek Tokarz <marek.tokarz14@gmail.com>
2025-06-14 20:49:55 +0200

    Simple program to count types and methods that are available

---
+++ Chapter02/Grammar/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//#error version
+using System.Reflection;
+
+Assembly? assembly = Assembly.GetEntryAssembly();
+
+if (assembly == null)
+{
+    return;
+}
+
+// loop to check assemblies that application refers to
+foreach (AssemblyName name in assembly.GetReferencedAssemblies())
+{
+    // load each assembly in order to read details about that assembly
+    var a = Assembly.Load(name);
+
+    // variable to sum all the methods
+    int numberOfMethods = 0;
+
+    // look to check all types from assembly
+    foreach (var t in a.DefinedTypes)
+    {
+        // sum a number of methods
+        numberOfMethods += t.GetMethods().Count();
+    }
+
+    // print a number of types and its methods
+    Console.WriteLine(
+        "In ASSEMBLY {0} there is {1} of types and {2} of methods.",
+        arg0: name.Name,
+        arg1: a.DefinedTypes.Count(),
+        arg2: numberOfMethods
+    );
+}
+
+Console.ReadKey();
