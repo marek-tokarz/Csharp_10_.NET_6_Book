d8fa9379d43859208b6f55e42212b044f9b37946
Marek Tokarz <marek.tokarz14@gmail.com>
2025-06-15 08:38:22 +0200

    Using binary, hexadecimal and _ notation with numbers

--- Chapter02/Grammar/Program.cs
+++ Chapter02/Grammar/Program.cs
@@ -47,4 +47,13 @@ Console.WriteLine($"Variable {nameof(heightInMeters)} has value {heightInMeters}
 // example with a file path
 string filePath = @"C:\tvs\sony\bravia.txt";
 
+// numbers notations
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+// checking if above numbers are equal
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
 Console.ReadKey();
